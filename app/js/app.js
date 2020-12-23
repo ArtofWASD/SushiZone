2fc8f5aafcd3c5f7e20737275c450b037022efba
Leonid <Chunlee666@gmail.com>
2020-12-23 21:14:12 +0300

    Готово на 98%

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,12 @@
 import $ from 'jquery'; window.jQuery = $; window.$ = $; // import module example (npm i -D jquery)
 require('../styles/libs/bootstrap-4.5.2-dist/js/bootstrap.bundle');
 require('../styles/libs/mmenu-light-master/dist/mmenu-light');
+require('../styles/libs/OwlCarousel2-2.3.4/dist/owl.carousel');
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
+    // Мобильное меню
     const menu = new MmenuLight(
         document.querySelector("#my-menu")
     );
@@ -18,5 +20,27 @@ document.addEventListener('DOMContentLoaded', () => {
             evnt.preventDefault();
             drawer.open();
         });
+    // карусуль слайдов
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        center:true,
+        margin:5,
+        dotsClass: false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            },
+            1000:{
+                items:5,
+                nav:true
+            }
+        }
+    });
 
 })
